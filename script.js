3f20012ed8352ae953dc7253a9474dc2aeb5b93f
keerati tod <keerati.tod@gmail.com>
2022-01-01 17:45:36 +0700

    add click button and styles

--- script.js
+++ script.js
@@ -1,64 +1,42 @@
 const selections = ["rock", "paper", "scissors"];
-let playerSelection;
-let computerSelection;
+const computerPlay = () => Math.floor(Math.random() * 3);
+
 let playerScore = 0;
 let computerScore = 0;
 
-function computerPlay() {
-  return selections[Math.floor(Math.random() * 3)];
+document.getElementById('rock').onclick = select;
+document.getElementById('paper').onclick = select;
+document.getElementById('scissors').onclick = select;
+
+function select() {
+  let playerSelection = this.id;
+  let computerSelection = selections[computerPlay()];  
+  let result = document.getElementById('result');
+  result.textContent = playRound(playerSelection, computerSelection);
+  if (playerScore == 5) {
+    result.textContent = `You are a Winner! Player ${playerScore} / Computer ${computerScore}`;
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore == 5) {
+    result.textContent = `You are a Loser! Player ${playerScore} / Computer ${computerScore}`;
+    playerScore = 0;
+    computerScore = 0;
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "rock" && computerSelection == "paper" ||
-      playerSelection == "paper" && computerSelection == "scissors" ||
+  if (playerSelection == computerSelection) {
+    return `Draw! Player ${playerScore} / Computer ${computerScore}`;
+  } else if (playerSelection == "rock" && computerSelection == "paper" ||
+      playerSelection == "paper" && computerSelection == "scissors" || 
       playerSelection == "scissors" && computerSelection == "rock") {
     computerScore += 1;
     return `You Lose! ${computerSelection} beats ${playerSelection}. Player ${playerScore} / Computer ${computerScore}`;
   } else if (playerSelection == "rock" && computerSelection == "scissors" ||
-            playerSelection == "paper" && computerSelection == "rock" ||
-            playerSelection == "scissors" && computerSelection == "paper") {
-      playerScore += 1;
-      return `You Win! ${playerSelection} beats ${computerSelection}. Player ${playerScore} / Computer ${computerScore}`;
-  } else if (playerSelection == computerSelection) {
-      return `Draw! Player ${playerScore} / Computer ${computerScore}`;
-  } else {
-      return 'THAT AINT COUNT!!!';
-  }
+      playerSelection == "paper" && computerSelection == "rock" || 
+      playerSelection == "scissors" && computerSelection == "paper") {
+    playerScore += 1;
+    return `You Win! ${playerSelection} beats ${computerSelection}. Player ${playerScore} / Computer ${computerScore}`;
+  } 
 }
 
-function game() {
-  playerSelection = prompt().toLowerCase();
-  computerSelection = computerPlay();
-  playRound();
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt().toLowerCase();
-  computerSelection = computerPlay();
-  playRound();
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt().toLowerCase();
-  computerSelection = computerPlay();
-  playRound();
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt().toLowerCase();
-  computerSelection = computerPlay();
-  playRound();
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = prompt().toLowerCase();
-  computerSelection = computerPlay();
-  playRound();
-  console.log(playRound(playerSelection, computerSelection));
-
-  if (playerScore > computerScore) {
-    return `You are the Winner! Player ${playerScore} / Computer ${computerScore}`;
-  } else if (playerScore < computerScore) {
-    return `You are the Loser! Player ${playerScore} / Computer ${computerScore}`;
-  } else {
-    return `You are the Drawer! Player ${playerScore} / Computer ${computerScore}`;
-  }
-}
-
-console.log(game());
